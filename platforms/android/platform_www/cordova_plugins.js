320f4f1a2cb5192ebfa4cbfc0053ee06707b7f4f
ngocsondns <dangngocson.dns@gmail.com>
2018-03-08 01:32:27 +0700

    Your Awesome Changes!

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,178 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "ionic-plugin-keyboard.keyboard",
+    "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+    "pluginId": "ionic-plugin-keyboard",
+    "clobbers": [
+      "cordova.plugins.Keyboard"
+    ],
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-email-composer.EmailComposer",
+    "file": "plugins/cordova-plugin-email-composer/www/email_composer.js",
+    "pluginId": "cordova-plugin-email-composer",
+    "clobbers": [
+      "cordova.plugins.email",
+      "plugin.email"
+    ]
+  },
+  {
+    "id": "onesignal-cordova-plugin.OneSignal",
+    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+    "pluginId": "onesignal-cordova-plugin",
+    "clobbers": [
+      "OneSignal"
+    ]
+  },
+  {
+    "id": "card.io.cordova.mobilesdk.CardIO",
+    "file": "plugins/card.io.cordova.mobilesdk/www/cdv-plugin-card-io.js",
+    "pluginId": "card.io.cordova.mobilesdk",
+    "clobbers": [
+      "CardIO"
+    ]
+  },
+  {
+    "id": "com.paypal.cordova.mobilesdk.PayPalMobile",
+    "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
+    "pluginId": "com.paypal.cordova.mobilesdk",
+    "clobbers": [
+      "PayPalMobile"
+    ]
+  },
+  {
+    "id": "cordova-plugin-stripe.stripe",
+    "file": "plugins/cordova-plugin-stripe/www/CordovaStripe.js",
+    "pluginId": "cordova-plugin-stripe",
+    "clobbers": [
+      "cordova.plugins.stripe"
+    ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-globalization.GlobalizationError",
+    "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+    "pluginId": "cordova-plugin-globalization",
+    "clobbers": [
+      "window.GlobalizationError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-globalization.globalization",
+    "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+    "pluginId": "cordova-plugin-globalization",
+    "clobbers": [
+      "navigator.globalization"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-apprate.AppRate",
+    "file": "plugins/cordova-plugin-apprate/www/AppRate.js",
+    "pluginId": "cordova-plugin-apprate",
+    "clobbers": [
+      "AppRate"
+    ]
+  },
+  {
+    "id": "cordova-plugin-apprate.locales",
+    "file": "plugins/cordova-plugin-apprate/www/locales.js",
+    "pluginId": "cordova-plugin-apprate",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-app-version.AppVersionPlugin",
+    "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
+    "pluginId": "cordova-plugin-app-version",
+    "clobbers": [
+      "cordova.getAppVersion"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-splashscreen": "4.1.0",
+  "ionic-plugin-keyboard": "2.2.1",
+  "cordova-plugin-email-composer": "0.8.15",
+  "onesignal-cordova-plugin": "2.3.1",
+  "cordova-plugin-ionic-webview": "1.1.16",
+  "cordova-plugin-whitelist": "1.3.3",
+  "card.io.cordova.mobilesdk": "2.1.0",
+  "com.paypal.cordova.mobilesdk": "3.5.0",
+  "cordova-plugin-stripe": "1.5.3",
+  "es6-promise-plugin": "4.2.2",
+  "cordova-plugin-x-socialsharing": "5.3.2",
+  "cordova-plugin-dialogs": "1.3.4",
+  "cordova-plugin-globalization": "1.0.9",
+  "cordova-plugin-inappbrowser": "1.7.2",
+  "cordova-plugin-apprate": "1.3.0",
+  "cordova-plugin-app-version": "0.1.9",
+  "cordova-plugin-statusbar": "2.4.2-dev",
+  "cordova-plugin-device": "1.1.7"
+};
+// BOTTOM OF METADATA
+});
---
+++ src/app/app.global.ts
@@ -0,0 +1,23 @@
+export const App: any = {
+  store           : 'Ionstore 2 - Tabs',  // change this with your app name
+  ContactEmail    : 'ionicpremium@gmail.com', // change this with your email contact
+  url             : 'https://princesswhite.rubi.vn', // change this with your URL (please use https, recommended)
+  OneSignalAppID  : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your onesignal api key
+  GCM             : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Google Project Number for Google Cloud Messaging
+  consumerKey     : 'ck_9d1ec5df84a85c7c2799b1b5630ef52903b5ca7c', // change this with your Consumer Key from WooCommerce
+  consumerSecret  : 'cs_67080a96b7f110d0b9eb0ca51c0c251a4a691fda', // change this with your Consumer Secret from WooCommerce
+
+  paypalSandboxClientID: 'AZjyISbp1zmOhZ0o_iAG3W2IGjlz2hvEC-8cGoQ7fXcMFN9afaRuW0X1B1PVSgkSuTQWOKqM9N4NTkOP',
+  paypalLiveClientID: '', // get this from paypal developer dashboard
+  paypalState: 'PayPalEnvironmentSandbox', // change this to 'PayPalEnvironmentProduction' if you wanna live
+
+  languages: [
+    {id: 'en', title: 'English'},
+    {id: 'id', title: 'Indonesian'},
+    {id: 'fr', title: 'French'},
+    {id: 'in', title: 'Hindi'},
+    {id: 'cn', title: 'Chinese'}
+  ],
+
+  defaultLang: 'en'
+};
